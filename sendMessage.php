a382ac1801745fcdae2274f1f5c2bbe50f0baa32
Shovon <mainulislam588@gmail.com>
2019-09-05 11:00:57 +0600

    server side for sending message

---
+++ sendMessage.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+require("connectToDB.php");
+
+$myEmail=$_SESSION['email'];
+$targetEmail=$_SESSION['targetEmail'];
+
+if(isset($_POST['send'])){
+    if(isset($_POST['text']) && !empty($_POST['text'])){
+
+        $text=$_POST['text'];
+        $time=time()+21600;
+
+        $q="insert into `message` (`who`,`withWho`,`time`,`message`) values('$myEmail','$targetEmail','$time','$text')";
+        $r=mysqli_query($conn,$q);
+        header("Location: temp.php");
+    }
+}
+
+
+?>

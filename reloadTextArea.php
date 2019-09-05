cf60807d8be91def53fe777d40b562437ac48d8d
Shovon <mainulislam588@gmail.com>
2019-09-05 11:00:43 +0600

    for auto refresh page

---
+++ reloadTextArea.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+require("connectToDB.php");
+
+$myEmail=$_SESSION['email'];
+$targetEmail=$_SESSION['targetEmail'];
+
+
+                        $query="select * from message where (`who`='$myEmail' and `withWho`='$targetEmail') or (`who`='$targetEmail' and `withWho`='$myEmail') order by `time` desc";
+                        $result=$conn->query($query);
+
+                        if($result->num_rows>0){
+                            while($row=$result->fetch_assoc()){
+                                $who=$row['who'];
+                                $withWho=$row['withWho'];
+                                $time=$row['time'];
+                                $msg=$row['message'];
+
+                                if($who==$myEmail){ ?>
+                                    <div class="container darker">
+                                        <img src="female.jpg" alt="Avatar" class="right" style="width:100%;">
+                                        <?php echo "".$msg; ?>
+                                        <span class="time-left"><?php echo "" . date("M d h:i A", $time); ?></span>
+                                    </div>
+                               
+                            <?php }
+                                else{ ?>
+                                    <div class="container">
+                                        <img src="male.png" alt="Avatar" style="width:100%;">
+                                        <?php echo "".$msg; ?>
+                                        <span class="time-right"><?php echo "" . date("M d h:i A", $time); ?> </span>
+                                    </div>
+
+                                
+                                <?php }
+                            }
+                        }
+
+?>

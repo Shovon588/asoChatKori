bffe36e6070811f2ba1abf1c94aec03b5e8d5257
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2019-11-12 18:17:38 +0600

    Update connectToDB.php

--- connectToDB.php
+++ connectToDB.php
@@ -1,23 +1,10 @@
 <?php 
 
-/*
 $dbServerName = "localhost";
 $dbServerUserID = "root";
 $dbServerPass = "";
 $dbName = "asochatkori";
 
 $conn = mysqli_connect( $dbServerName , $dbServerUserID , $dbServerPass , $dbName  );
-*/
 
-
-
-$dbServerName = "intern-sls.cdts6wfxxv6z.eu-central-1.rds.amazonaws.com";
-    $dbServerUserID = "slsadmin";
-    $dbServerPass = "EMSAFNgw04ljnyKN4";
-    $dbName = "asochatkori";
-
-    $conn = mysqli_connect( $dbServerName , $dbServerUserID , $dbServerPass , $dbName  );
-
-
-?> 
-
+?>
